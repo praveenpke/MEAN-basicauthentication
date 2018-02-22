3aed677ee18e0ffd21e191585003d791cc3e95e9
praveen emani <praveenemani@praveens-MacBook-Pro.local>
2018-02-22 00:16:44 -0600

    setup routes,cors middleware, static folder served, database connected

--- app.js
+++ app.js
@@ -4,10 +4,30 @@ const bodyParser = require('body-parser');
 const cors  = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config  = require('./config/database');
+
+//connecting to database mlab cloud
+mongoose.connect(config.database);
+
+
 
 const app = express();
 
+const users = require('./routes/users');
 
+//CORS allows us to make request to this api from different domain
+app.use(cors());
+
+//parse the json in incoming request
+app.use(bodyParser.json());
+
+
+//Set static client folder
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/users',users);
+
+//Default Route
 app.get('/',(req,res)=>{
     res.send("You got this message from Express server");
 });
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: 'mongodb://user:123@ds243768.mlab.com:43768/praveenauthapp',
+    secret:'secretToken'
+}
