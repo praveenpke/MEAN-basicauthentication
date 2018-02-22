a16a91e0ce90c1a03a11d88136c2f2384e806a3c
praveen emani <praveenemani@praveens-MacBook-Pro.local>
2018-02-22 00:18:12 -0600

    setup routes,cors middleware, static folder served, database connected

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //Register
-router.get('/register',(req,res,next)=>{
+router.post('/register',(req,res,next)=>{
     res.send('REGISTER');
 });
 
@@ -17,9 +17,5 @@ router.get('/profile',(req,res,next)=>{
 });
 
 
-//VALIDATE
-router.get('/validate',(req,res,next)=>{
-    res.send('VALIDATE');
-});
 
 module.exports = router;
